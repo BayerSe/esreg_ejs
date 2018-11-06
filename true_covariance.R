7608646f2f29d5b693bb366e25960f75e840b1f1
Sebastian Bayer <bayerse@gmail.com>
2018-11-06 22:17:27 +0100

    Increase sample size

--- true_covariance.R
+++ true_covariance.R
@@ -12,7 +12,7 @@ dir.create(path, showWarnings = FALSE, recursive = TRUE)
 # Simulation setup
 setup <- expand.grid(
   alpha   = 0.025,
-  n       = 10^5, # 9
+  n       = 10^9,
   design  = c(1, 2, 3, 4),
   g1      = c(1, 2),
   g2      = c(1, 2, 3, 4, 5)
