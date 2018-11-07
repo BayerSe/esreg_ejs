ceaa50a36c52eb09100d7ad7c65913c8f92059a2
sebastian.bayer@uni-konstanz.de <kn_pop260093@uc1n996.localdomain>
2018-11-07 19:26:16 +0100

    update run files

---
+++ run/job_true_covariance.sh
@@ -0,0 +1,2 @@
+module load math/R/3.5.1
+Rscript -e "source('../true_covariance.R', chdir = TRUE)"
---
+++ run/run_job_true_covariance.sh
@@ -0,0 +1,1 @@
+msub -N true_covariance -q singlenode -l nodes=1:ppn=4 -l walltime=1:00:00:00 -l pmem=16000mb job_true_covariance.sh
