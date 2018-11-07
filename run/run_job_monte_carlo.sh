be8c9d860cf8896f2fb3fd35e0f34d14b5d8655e
sebastian.bayer@uni-konstanz.de <kn_pop260093@uc1n996.localdomain>
2018-11-07 22:10:51 +0100

    add loop

--- run/run_job_monte_carlo.sh
+++ run/run_job_monte_carlo.sh
@@ -1,1 +1,3 @@
-msub -N true_covariance -q singlenode -l nodes=1:ppn=16 -l walltime=00:12:00:00 job_monte_carlo.sh
+for i in {1..10}
+  do msub -N true_covariance -q singlenode -l nodes=1:ppn=16 -l walltime=00:12:00:00 job_monte_carlo.sh
+done
