fbfe8536f5009b0c8be41a758cc4f0278ff50c7e
sebastian.bayer@uni-konstanz.de <kn_pop260093@uc1n997.localdomain>
2018-11-10 23:07:44 +0100

    increase walltime

--- run/run_job_true_covariance.sh
+++ run/run_job_true_covariance.sh
@@ -1,1 +1,1 @@
-msub -N true_covariance -q fat -l nodes=1:ppn=16 -l walltime=00:12:00:00 -l pmem=64000mb job_true_covariance.sh
+msub -N true_covariance -q fat -l nodes=1:ppn=16 -l walltime=01:00:00:00 -l pmem=64000mb job_true_covariance.sh
