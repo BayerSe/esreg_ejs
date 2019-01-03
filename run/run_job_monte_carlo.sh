77eb2cac58c0b7cebce111baa8d1d5d3fef22ad4
sebastian.bayer@uni-konstanz.de <kn_pop260093@uc1n997.localdomain>
2019-01-03 22:23:17 +0100

    adjust run par

--- run/run_job_monte_carlo.sh
+++ run/run_job_monte_carlo.sh
@@ -1,3 +1,3 @@
-for i in {1..10}
-  do msub -N true_covariance -q singlenode -l nodes=1:ppn=4 -l walltime=01:00:00:00 job_monte_carlo.sh
+for i in {1..50}
+  do msub -N true_covariance -q singlenode -l nodes=1:ppn=16 -l walltime=03:00:00:00 job_monte_carlo.sh
 done
--- run/run_job_true_covariance.sh
+++ run/run_job_true_covariance.sh
@@ -1,1 +1,2 @@
-msub -N true_covariance -q fat -l nodes=1:ppn=16 -l walltime=02:00:00:00 -l pmem=64000mb job_true_covariance.sh
+#msub -N true_covariance -q fat -l nodes=1:ppn=16 -l walltime=02:00:00:00 -l pmem=64000mb job_true_covariance.sh
+msub -N true_covariance -l nodes=1:ppn=4 -l walltime=02:00:00:00 -l pmem=10000mb job_true_covariance.sh
